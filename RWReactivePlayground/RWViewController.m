e5d1e2f0b8f0732d05998961ad003cfd9054f8e4
wangtao <wangtaosecret@gmail.com>
2015-07-08 19:12:59 +0800

    So cool.ReactiveCocoa Tutorial from http://www.raywenderlich.com/62699/reactivecocoa-tutorial-pt1

---
+++ RWReactivePlayground/RWViewController.m
@@ -0,0 +1,100 @@
+//
+//  RWViewController.m
+//  RWReactivePlayground
+//
+//  Created by Colin Eberhardt on 18/12/2013.
+//  Copyright (c) 2013 Colin Eberhardt. All rights reserved.
+//
+
+#import "RWViewController.h"
+#import "RWDummySignInService.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+@interface RWViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *signInButton;
+@property (weak, nonatomic) IBOutlet UILabel *signInFailureText;
+
+//@property (nonatomic) BOOL passwordIsValid;
+//@property (nonatomic) BOOL usernameIsValid;
+@property (strong, nonatomic) RWDummySignInService *signInService;
+
+@end
+
+@implementation RWViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  
+  self.signInService = [RWDummySignInService new];
+  // initially hide the failure message
+  self.signInFailureText.hidden = YES;
+    
+    
+    
+    
+    RACSignal *validUserNameSignal = [self.usernameTextField.rac_textSignal map:^id(NSString* name) {
+        return @([self isValidUsername:name]);
+    }];
+    
+    RACSignal *validPasswordSignal = [self.passwordTextField.rac_textSignal map:^id(NSString* password) {
+        return @([self isValidPassword:password]);
+    }];
+    RAC(self.passwordTextField,backgroundColor) =
+    [validPasswordSignal map:^id(NSNumber* passwordValid) {
+        return [passwordValid boolValue]?  [UIColor clearColor] : [UIColor yellowColor];
+    }];
+    
+    RAC(self.usernameTextField,backgroundColor) =
+    [validUserNameSignal map:^id(NSNumber* usernameValid) {
+            return [usernameValid boolValue]?[UIColor clearColor] : [UIColor yellowColor];
+    }];
+    
+    RACSignal *signUpActiveSignal =
+    [RACSignal combineLatest:@[validUserNameSignal,validPasswordSignal] reduce:^id(NSNumber* usernameValid,NSNumber* passwordValid){
+        return @([usernameValid boolValue] && [passwordValid boolValue]);
+    }];
+    [signUpActiveSignal subscribeNext:^(NSNumber *signupActive) {
+        self.signInButton.enabled = [signupActive boolValue];
+    }];
+    
+    
+    [[[[self.signInButton rac_signalForControlEvents:UIControlEventTouchUpInside]
+      doNext:^(id x) {
+          self.signInButton.enabled = NO;
+          self.signInFailureText.hidden = YES;
+      }]
+    flattenMap:^id(id value) {
+        return [self signInSignal];
+    }]
+    subscribeNext:^(NSNumber* signedIn) {
+        BOOL success = [signedIn boolValue];
+        self.signInFailureText.hidden = success;
+        if (success) {
+            [self performSegueWithIdentifier:@"signInSuccess" sender:self];
+        }
+//        NSLog(@"Sign in result :%@",x);
+    }];
+}
+
+- (BOOL)isValidUsername:(NSString *)username {
+  return username.length > 3;
+}
+
+- (BOOL)isValidPassword:(NSString *)password {
+  return password.length > 3;
+}
+- (RACSignal * )signInSignal
+{
+    return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+        [self.signInService signInWithUsername:self.usernameTextField.text password:self.passwordTextField.text complete:^(BOOL success) {
+            [subscriber sendNext:@(success)];
+            [subscriber sendCompleted];
+        }];
+        return nil;
+    }];
+}
+
+@end
